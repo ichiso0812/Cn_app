128ca9e1d5df2d4b3cc65ef33f5382773846a181
Jean Takanacare <luka.ouddanne@gmail.com>
2020-02-27 14:17:11 +0100

    Luka

--- partials/_all_series_slider.php
+++ partials/_all_series_slider.php
@@ -2,15 +2,20 @@
     $bdd = new PDO('mysql:host=localhost;dbname=cn-watch;charset=utf8', "root", "", array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
     $series = $bdd->query('SELECT id,name, ico_link FROM serie');
 ?>
-
+<style>
+    #all_series_slider img{
+        margin-right: 15px;
+        max-height: 75px;
+    }
+</style>
 <div id="all_series_slider">
     <?php while ($serie = $series->fetch()){ ?>
-        <div data-id=<?=$serie['id'];?>>
+        <div class="slide" data-id=<?=$serie['id'];?>>
             <a href=<?='/Cn-app/pages/serie?id=' . $serie['id'];?>>
                 <img class="img-fluid" src=<?='/Cn-app' . $serie['ico_link'];?> alt=<?=$serie['name'];?>>
             </a>
-            
         </div>
-    <?php } 
+    <?php } ?>
+
     $series->closeCursor(); ?>
 </div>
